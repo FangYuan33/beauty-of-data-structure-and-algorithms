37c127c194de96b32af3fce9deeaa9b50ef2a37a
FangYuan <374072213@qq.com>
2023-07-11 21:00:09 +0800

    链表课后练习28

---
+++ src/main/java/com/algorithms/linkedlist/Exercise28.java
@@ -0,0 +1,33 @@
+package com.algorithms.linkedlist;
+
+/**
+ * 链表课后练习28
+ *
+ * @author FangYuan
+ * @since 2023-07-11 20:57:18
+ */
+public class Exercise28 {
+
+    public static void main(String[] args) {
+        ListNode node = ListNodeUtil.getFixLengthListNode(5, 9);
+        new Exercise28().max(node);
+
+        System.out.println(max);
+    }
+
+    private static int max = 0;
+
+    /**
+     * 递归法解决27题
+     * @param node
+     */
+    private void max(ListNode node) {
+        // 递归结束条件
+        if (node == null) {
+            return;
+        }
+
+        max = Math.max(max, node.val);
+        max(node.next);
+    }
+}
