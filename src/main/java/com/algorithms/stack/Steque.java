3c29306d403b6f2c707b82decc42fc52d8e7d3d2
FangYuan <374072213@qq.com>
2023-07-12 22:03:36 +0800

    Steque

---
+++ src/main/java/com/algorithms/stack/Steque.java
@@ -0,0 +1,97 @@
+package com.algorithms.stack;
+
+import com.algorithms.linkedlist.ListNode;
+import com.algorithms.linkedlist.ListNodeUtil;
+
+/**
+ * 支持 push pop dequeue 操作的数据结果
+ *
+ * @author FangYuan
+ * @since 2023-07-12 21:44:32
+ */
+public class Steque {
+
+    public static void main(String[] args) {
+        Steque steque = new Steque();
+
+        steque.push(1);
+        steque.enqueue(0);
+        steque.push(2);
+        ListNodeUtil.print(steque.head);
+
+        System.out.println(steque.pop());
+        System.out.println(steque.pop());
+        System.out.println(steque.pop());
+    }
+
+    private ListNode head;
+
+    private ListNode tail;
+
+    private int size;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 尾插法
+     */
+    public void push(int val) {
+        if (isEmpty()) {
+            head = new ListNode(val);
+            tail = head;
+        } else {
+            tail.next = new ListNode(val);
+            tail = tail.next;
+        }
+
+        size++;
+    }
+
+    /**
+     * 头插法
+     */
+    public void enqueue(int val) {
+        if (isEmpty()) {
+            head = new ListNode(val);
+            tail = head;
+        } else {
+            ListNode newHead = new ListNode(val);
+            newHead.next = head;
+            head = newHead;
+        }
+
+        size++;
+    }
+
+    /**
+     * 出去
+     */
+    public Integer pop() {
+        if (isEmpty()) {
+            return null;
+        } else if (size == 1) {
+            int result = head.val;
+            head = null;
+            tail = null;
+            size--;
+
+            return result;
+        } else {
+            ListNode node = head;
+            while (node.next.next != null) {
+                node = node.next;
+            }
+            int result = node.next.val;
+            node.next = null;
+            size--;
+
+            return result;
+        }
+    }
+}
